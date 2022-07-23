8ba31fe00f2dc74166edbc943f50649f02c7a332
Craig Ford <craig_ford@icloud.com>
2022-07-23 16:43:26 -0400

    commit tree 0b8ed555d524333c5e0f0e04688c586deca17fe9

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
-# Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+greet "Naureen"
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+greet_with_default "Naureen" 
+greet_with_default 
+
+def add(num1, num2)
+  num1 + num2
+end
+
+sum = add(1, 2)
+puts sum
+
+def halve(num3)
+    if num3.class != Integer
+      return nil
+    end
+    num3 / 2
+end
+
+my_halve = halve(4)
+puts my_halve
